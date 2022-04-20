a47d55022bb4544ed161410d623ada00e1747f35
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-04-20 15:34:23 +0530

    Time: 10 ms (66.62%), Space: 11.8 MB (61.28%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<string> gstring = generate(n);
-        vector<int> ans ;
-        for(auto i: gstring){
-            ans.push_back(stoi(i,0,2));
-        }
-        
+       vector<int> ans;
+        for(int i = 0; i < 1 << n; i++) 
+			/*
+			As we know that MSB remains the same and all other bits are XOR of current and next bit so
+			we can right shift i by 1 unit and take XOR with i to get desried result
+			*/
+            ans.push_back(i ^ (i >> 1));
         return ans;
     }
-    vector<string> generate(int n){
-        if(n==1)
-            return {"0","1"};
-        
     
-    vector<string> ans;
-    vector<string> temp = generate(n-1);
-    
-    for(int i=0;i<temp.size();i++){
-        ans.push_back("0"+temp[i]);
-        
-    }
-      for(int i=temp.size()-1;i>=0;i--){
-          ans.push_back("1"+temp[i]);
-          
-      }
-    return ans;
-}
+
     
 };
