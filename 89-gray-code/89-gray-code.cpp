60284823d3e7341a7b2b10fdbde2b67cf743928d
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-04-20 15:33:05 +0530

    Time: 99 ms (5.99%), Space: 39.7 MB (8.70%) - LeetHub

---
+++ 89-gray-code/89-gray-code.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> grayCode(int n) {
+        vector<string> gstring = generate(n);
+        vector<int> ans ;
+        for(auto i: gstring){
+            ans.push_back(stoi(i,0,2));
+        }
+        
+        return ans;
+    }
+    vector<string> generate(int n){
+        if(n==1)
+            return {"0","1"};
+        
+    
+    vector<string> ans;
+    vector<string> temp = generate(n-1);
+    
+    for(int i=0;i<temp.size();i++){
+        ans.push_back("0"+temp[i]);
+        
+    }
+      for(int i=temp.size()-1;i>=0;i--){
+          ans.push_back("1"+temp[i]);
+          
+      }
+    return ans;
+}
+    
+};
