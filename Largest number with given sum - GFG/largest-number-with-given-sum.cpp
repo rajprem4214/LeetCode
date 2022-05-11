8a0d461d7b4c7facc925c577edea370b0a0d5aed
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-05-11 17:47:40 +0530

    Added solution - LeetHub

---
+++ Largest number with given sum - GFG/largest-number-with-given-sum.cpp
@@ -0,0 +1,49 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// Function to return Largest Number
+
+class Solution
+{
+    public:
+    //Function to return the largest possible number of n digits
+    //with sum equal to given sum.
+    string largestNumber(int n, int sum)
+    {
+        // Your code here
+        string ans="";
+        if(9*n < sum)
+        return "-1";
+        
+        while(ans.size() < n)
+        {
+            int dig = min(sum,9);
+            sum-=dig;
+            ans+=to_string(dig);
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking testcases
+	int t;
+	cin>>t;
+
+	while(t--)
+	{
+	    //taking n and sum
+		int n,sum;
+		cin>>n>>sum;
+		
+        Solution obj;
+        //function call
+		cout<<obj.largestNumber(n, sum)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
