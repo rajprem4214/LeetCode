b465277959e5ea4ea6fc9bbffcc57f96e9674ce2
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-06-18 01:33:31 +0530

    Time: 59 ms (48.32%), Space: 89.4 MB (62.24%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -2,38 +2,51 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         
-    
-vector<int>num(nums1.size()+nums2.size());
-int i=0,k=0,j=0;
-while(i<nums1.size() && j<nums2.size())
-{
-if(nums1[i]<nums2[j])
-{
-num[k++]=nums1[i++];
-}
-else
-{
-num[k++]=nums2[j++];
-}
-}
-while(i<nums1.size())
-{
-num[k++]=nums1[i++];
-}
-while(j<nums2.size())
-{
-num[k++]=nums2[j++];
-}
-int mid=(nums1.size()+nums2.size())/2;
-double median;
-if((nums1.size()+nums2.size())%2==0)
-{
-median=(num[mid]+num[mid-1])/2.0;
-}
-else
-{
-median=num[mid];
-}
-return median;
-}
+        int l1 = nums1.size();
+        int l2 = nums2.size();
+        int i =  0;
+        int j = 0;
+        int f[l1+l2];
+        int k =0;
+        while(i<l1 && j<l2){
+            if(nums1[i] < nums2[j])
+            {
+                f[k++] = nums1[i++];
+                
+            }
+            else 
+            {
+               f[k++] = nums2[j++];
+                
+            }
+            
+            
+        }
+        while(i<l1)
+        {
+            f[k++] = nums1[i++];
+            
+        }
+        
+        while(j<l2){
+            f[k++] = nums2[j++];
+            
+        }
+        double ans = 0.0;
+            int mid = (l1+l2)/2;
+        if((nums1.size()+nums2.size()) % 2 ==0)
+        {
+            
+            
+            ans =(f[mid-1] + f[mid])/2.0;
+        } 
+        else
+        {
+         //   int mid = (l1+l2)/2;
+            ans = f[mid];
+        }
+        
+        return ans;
+        
+    }
 };
