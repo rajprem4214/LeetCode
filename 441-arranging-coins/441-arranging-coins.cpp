46ae8f5b97ba88d44664d4e51e7e66807cea4114
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-09-12 11:12:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (69.19%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+class Solution {
+public:
+    int arrangeCoins(int n) {
+       ll int l = 1, h = n;
+       ll int ans = -1;
+        while(l<=h)
+        {
+           ll int m = (l+h)/2;
+            if(m*(m+1)/2 <= n)
+            {
+                ans=m;
+                l=m+1;
+            }
+            else
+                h=m-1;
+        }
+        return ans;
+    }
+};
