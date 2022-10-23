a5ed9e9d3ee0535090567e7e7c86cb878bc23886
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-10-23 18:54:31 +0530

    Time: 569 ms (22.22%), Space: 86.8 MB (22.22%) - LeetHub

---
+++ 2449-minimum-number-of-operations-to-make-arrays-similar/2449-minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long makeSimilar(vector<int>& nums, vector<int>& target) {
+       vector<int> even1, even2;
+        vector<int> odd1, odd2;
+        
+        sort(nums.begin(), nums.end());
+        sort(target.begin(), target.end());
+        
+        for (int i=0; i<nums.size(); i++) {
+            if (nums[i]%2) odd1.push_back(nums[i]);
+            else even1.push_back(nums[i]);
+            
+            if (target[i]%2) odd2.push_back(target[i]);
+            else even2.push_back(target[i]);
+        }
+        
+        long long res=0;
+        for (int i=0; i<even1.size(); i++) {
+            if (even1[i]<even2[i])
+                res+=(even2[i]-even1[i])/2;
+        }
+        for (int i=0; i<odd1.size(); i++) {
+            if (odd1[i]<odd2[i])
+                res+=(odd2[i]-odd1[i])/2;
+        }
+        
+        return res; 
+    }
+};
