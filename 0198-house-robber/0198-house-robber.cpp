4aa3f2bd21adfc2941ce4c11a52c075d4889bffc
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2022-11-16 22:44:12 +0530

    Time: 3 ms (56.94%), Space: 7.9 MB (6.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int f(int ind,vector<int>& nums,vector<int>&dp){
-        if(ind==0)
-            return nums[0];
-        if(ind<0)
-            return 0;
+//     int f(int ind,vector<int>& nums,vector<int>&dp){
+//         if(ind==0)
+//             return nums[0];
+//         if(ind<0)
+//             return 0;
         
-        if(dp[ind] != -1)
-            return dp[ind];
+//         if(dp[ind] != -1)
+//             return dp[ind];
         
-            int take = nums[ind] + f(ind-2,nums,dp);
-            int not_take = f(ind-1,nums,dp);
+//             int take = nums[ind] + f(ind-2,nums,dp);
+//             int not_take = f(ind-1,nums,dp);
         
-        return dp[ind]= max(take, not_take);
-    }
+//         return dp[ind]= max(take, not_take);
+//     }
     
     int rob(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n+1,-1);
-     return f(n-1,nums,dp);
+         if(n==1) return nums[0];
+        // vector<int> dp(n+1,-1);
         
+          vector<int>dp(n,-1);
+  dp[0]=nums[0];
+  dp[1]=nums[1];
+  dp[1]=max(dp[0],dp[1]);
+  for(int i=2;i<n;i++){
+    int incl=dp[i-2]+nums[i];
+    int excl=dp[i-1]+0;
+  dp[i]=max(incl,excl);
+  }
+  return dp[n-1];
     }
 };
