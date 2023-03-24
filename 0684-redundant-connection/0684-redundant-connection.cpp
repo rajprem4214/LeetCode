9345ecb8202900d6cd09e56a82139e698f871205
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2023-03-24 11:12:55 +0530

    Time: 30 ms (9.39%), Space: 33.1 MB (6.06%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>> &adj,vector<int> &visited,int v,int parent){
+        visited[v]=1;
+        for(int i=0;i<adj[v].size();i++){
+            if(!visited[adj[v][i]]){
+                if(dfs(adj,visited,adj[v][i],v)){
+                    return true;
+                }
+            }else if(parent!=adj[v][i]){
+                return true;
+            }
+        }
+        return false;
+        // visited[v] = 0;
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> res;
+        int n=edges.size();
+        int minV=INT_MAX;
+        vector<vector<int>> adj(n+1);
+       
+        for(int i=0;i<n;i++){
+            adj[edges[i][0]].push_back(edges[i][1]);
+            adj[edges[i][1]].push_back(edges[i][0]);
+             vector<int> visited(n+1,0);
+            if(dfs(adj,visited,edges[i][0],-1)){
+                res={edges[i][0],edges[i][1]};
+                break;
+            }
+        }
+        return res;
+    }
+};
