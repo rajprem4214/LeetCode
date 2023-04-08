b67743049b0aa4d816c7de8cf275533274f69a44
Prem Raj <85401522+rajprem4214@users.noreply.github.com>
2023-04-08 22:42:03 +0530

    Time: 4 ms (10.75%), Space: 6.1 MB (49.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     // recursive dp
-    
-    int f(int n, vector<int>&dp)
-    {
-        if(n==0)
-            return 1;
-        if(n<0)
-            return 0;
+//     int f(int n, vector<int>&dp)
+//     {
+//         if(n==0)
+//             return 1;
+//         if(n<0)
+//             return 0;
         
-        if(dp[n] != -1)
-            return dp[n];
+//         if(dp[n] != -1)
+//             return dp[n];
         
-         int one = f(n-1, dp);
-        int two = 0;
-        if(n>1)
-         two = f(n-2, dp);
+//          int one = f(n-1, dp);
+//         int two = 0;
+//         if(n>1)
+//          two = f(n-2, dp);
         
-        return dp[n] = one+two;
+//         return dp[n] = one+two;
         
-    }
+//     }
     
     
     int climbStairs(int n) {
         
         vector<int> dp(n+1,-1);
-        return f(n, dp);
+        // return f(n, dp);
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+        
     }
 };
